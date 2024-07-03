71c95c43cb2f96c18b794d4a9f4bd57531402651
Minsoo Cho <textign@gmail.com>
2024-07-03 12:51:58 +0900

    api 주소 변경

--- src/queries/pokemons.ts
+++ src/queries/pokemons.ts
@@ -2,11 +2,11 @@ import { Pokemon } from "@/types/pokemons.type";
 import axios from "axios";
 
 export async function getAllPokemons(): Promise<Pokemon[]> {
-  const { data } = await axios.get("http://localhost:3000/api/pokemons");
+  const { data } = await axios.get("https://pokemons-one-xi.vercel.app/api/pokemons");
   return data;
 }
 
 export async function getPokemon(id: string): Promise<Pokemon> {
-  const { data } = await axios.get(`http://localhost:3000/api/pokemons/${id}`);
+  const { data } = await axios.get(`https://pokemons-one-xi.vercel.app/api/pokemons/${id}`);
   return data;
 }
